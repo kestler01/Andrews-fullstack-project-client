fab1c62946e440c5d753474f55f629fdf480b53c
kestler01 <kestler.andrew@gmail.com>
2021-08-10 09:56:19 -0400

    Committing changes to ui.js
    Trying to make the sign in request trigger a retrieval for the users pieces and use some js to make a card per piece on the view. commiting to push to repository to check functionality

--- app/ui.js
+++ app/ui.js
@@ -1,6 +1,24 @@
 const store = require('./store.js')
 const Modal = require('bootstrap').Modal
 
+const drawPieceCard = function (piece) {
+  // look up the piece data, ! - would be a separate api call for each !?! f no- must populate that array on call first.
+  const { name, medium, description, link } = piece
+  // make a card with the data,
+  // append card into the page @ "#pieces-landing-pad"
+  $('#pieces-landing-pad').append(`
+    <div class="card">
+  <img src="${link}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${name} : ${medium} </h5>
+    <p class="card-text">${description}</p>
+    <a href="#" class="btn btn-primary"> open modal of piece update form  </a> // this button will be to trigger an edit form (will allow patch and delete)
+    <a href="#" class="btn btn-primary"> open modal of piece to blow up image </a> // this button will trigger a script to create the modal, and it's close button will destroy it
+  </div>
+</div>
+`)
+}
+
 const onSignUpSuccess = function () {
   $('#sign-up').trigger('reset')
   $('#sign-up-form-message').text('sign up successful, log in to continue')
@@ -25,16 +43,12 @@ const onSignInSuccess = function (response) {
   $('#update-profile-username-field').val(response.user.userName)
   $('#update-profile-mediums-field').val(response.user.mediums)
   $('#update-profile-bio-field').val(response.user.bio)
-  // need to get user data from api, and change view to user homepage
+  // need to get user data from api, and change view to user 'homepage'
   store.user = response.user
-  store.user.pieces
-    .forEach(
-    // look up the piece data, ! - would be a seperate api call for each !?! f no- must popluate that array on call first.
-
-    // make a card with the data,
-    // append card into the page @ "#pieces-landing-pad"
-    )
-  // console.log(store)
+  if (store.user.pieces !== []) {
+    store.user.pieces.forEach(piece => drawPieceCard(piece)) // here we iterate over the array, but I could start an index request instead
+  }
+  console.log(store)
   myModal._hideModal() // do not know why but hide and toggle methods are not working in this scenario with a submit and reset of the form before the hide.
   $('.modal-backdrop').hide() // janky but traditional modal methods aren't working here for some reason
 }
